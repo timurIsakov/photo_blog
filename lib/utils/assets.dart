77f02b0cfc1367c4ae2b20b2f2847a1c5c6442ec
Timur Isakov <timurisakov@192.168.1.14>
2025-02-23 21:29:29 +0500

    connected images

---
+++ lib/utils/assets.dart
@@ -0,0 +1,14 @@
+class Assets {
+  //PNG
+  static const String tGirlAvatar = 'assets/images/girl_avatar.png';
+  static const String tGirlImage = 'assets/images/girl_image.png';
+  static const String tHistoryBackground =
+      'assets/images/history_background.png';
+  static const String tMainBackground = 'assets/images/main_background.png';
+  static const String tManAvatar = 'assets/images/man_avatar.png';
+  static const String tManImage = 'assets/images/man_image.png';
+  static const String tVillaImage = 'assets/images/villa_image.png';
+
+  //SVG
+  static const String tMainLogo = 'assets/images/main_logo.svg';
+}
